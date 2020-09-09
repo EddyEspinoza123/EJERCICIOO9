518b0f026ffdbacd0a1d5234597a2c0b4e15d952
EddyEspinoza123 <68787942+EddyEspinoza123@users.noreply.github.com>
2020-09-09 17:30:45 -0500

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ejercicio 9
+            List<int> t = Enumerable.Range(1, 50).ToList();
+            var pares = t.Where(num => num % 2 == 0);
+            foreach (int i in pares)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
